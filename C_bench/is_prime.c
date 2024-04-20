96f02a7b70580d5cd1233e0c468e235f4633ac21
YoavLevi <60928603+YoavLevi@users.noreply.github.com>
2024-04-20 12:49:25 -0700

    Add files via upload

---
+++ C_bench/is_prime.c
@@ -0,0 +1,21 @@
+int IsPrime(unsigned long long int n)
+{
+    if (n == 2 || n == 3)
+        return 1;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return 1;
+
+    for (unsigned long long int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    IsPrime(8225092069056351469);
+}
